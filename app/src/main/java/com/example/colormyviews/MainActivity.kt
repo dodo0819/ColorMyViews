c83ac79287b7c011f2ffec17ae1dbeb783152a60
du wei <duwei583@gmail.com>
2019-11-06 14:57:16 +0800

    setbackgroundresource

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -17,13 +17,20 @@ class MainActivity : AppCompatActivity() {
     private fun makeColored(view: View) {
         when (view.id) {
 
-            // Boxes using Color class colors for the background
+            /*// Boxes using Color class colors for the background
             R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
             R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
             R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
-            else -> view.setBackgroundColor(Color.LTGRAY)
+            else -> view.setBackgroundColor(Color.LTGRAY)*/
+
+            R.id.box_one_text -> view.setBackgroundResource(R.drawable.one)
+            R.id.box_two_text -> view.setBackgroundResource(R.drawable.two)
+            R.id.box_three_text -> view.setBackgroundResource(R.drawable.three)
+            R.id.box_four_text -> view.setBackgroundResource(R.drawable.four)
+            R.id.box_five_text -> view.setBackgroundResource(R.drawable.five)
+            else -> view.setBackgroundResource(R.drawable.zero)
         }
     }
 
